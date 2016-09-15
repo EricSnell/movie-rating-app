c20ac32cc234880dca20b487c81510ac85d228b4
Sierra Gregg <sierragregg@Gmail.com>
2016-09-15 15:18:02 -0500

    Added search bar to nav

--- src/js/components/Navigation.jsx
+++ src/js/components/Navigation.jsx
@@ -23,24 +23,35 @@ class Navigation extends React.Component {
         }
 
         return (
-            <nav className="navbar navbar-default navbar-static-top">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <li>
-                            <Link to={'/'}>Logo</Link>
-                        </li>
+            <header>
+                <nav className="navbar navbar-default">
+                    <div className="container">
+                        <Link className="navbar-left" to={'/'}>
+                            <img
+                              src="http://demo.vuchkov.biz/theme2/img/blank_logo.gif"
+                              alt="logo"
+                              style={{maxWidth:"25%", maxHeight:"auto"}}
+                              />
+                        </Link>
+                        <form className="navbar-form navbar-right">
+                            <div className="form-group">
+                                <input type="text" placeholder="Movie Title" className="search-bar form-control"/>
+                            </div>
+                        </form>
+                        <ul className="nav navbar-nav navbar-right" style={{
+                            padding: "0 2% 0 2%"
+                        }}>
+                            <li onClick={this.signUp}>{link}</li>
+                        </ul>
                     </div>
-                    <ul className="nav navbar-nav"  style={{padding: "0 2% 0 2%"}}>
-                        <li onClick={this.signUp}>{link}</li>
-                    </ul>
-                </div>
-            </nav>
-        )
+                </nav>
+            </header>
+        );
     }
 }
 
-var mapStateToProps = (state, props) => {
+const mapStateToProps = (state, props) => {
     return {loggedIn: state.loggedIn, mainUserId: state.mainUserId}
-}
+};
 
 export default connect(mapStateToProps)(Navigation)
